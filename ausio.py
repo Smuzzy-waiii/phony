4c36f59c6c4e93c02420b2aa5ff600c49225115e
Smaran Jawalkar <smaran.jawalkar@gmail.com>
2023-03-29 12:12:35 +0530

    added debugging

--- ausio.py
+++ ausio.py
@@ -3,10 +3,12 @@
 import sounddevice as sd
 from UDPClientHelper import UDPHelper
 import sys
+import numpy as np
+
 my_port = int(sys.argv[1])
 not_my_port = int(sys.argv[2])
 
-duration = 15.5  # seconds
+duration = 60  # seconds
 block_size = 2048
 sample_rate = 44100  # Sample rate in Hz
 
@@ -15,13 +17,27 @@ udp_service = UDPHelper("localhost", not_my_port, my_port)
 def callback(indata, outdata, frames, time, status):
     if status:
         print(status)
-    #outdata[:] = indata
+
+    # Send audio data to remote client
     udp_service.sendPacket(indata.tobytes())
+    print(f"Sent {len(indata.tobytes())} bytes to remote client")
+
+    # Receive audio data from remote client
     message, clientAddress = udp_service.recv(block_size*4)
+    print(f"Received {len(message)} bytes from remote client")
+
+    # Convert received data to numpy array
     data = np.frombuffer(message, dtype="float32")
     data = data.reshape((len(data), 1))
+
+    # Output received data to speaker
     outdata[:] = data
-    # print(indata.dtype, indata.shape)
+
+    # Debugging output
+    print(f"indata shape: {indata.shape}")
+    print(f"outdata shape: {outdata.shape}")
+    print(f"data shape: {data.shape}")
+
 
 with sd.Stream(channels=1, samplerate=sample_rate, blocksize=block_size, callback=callback):
     sd.sleep(int(duration * 1000))
